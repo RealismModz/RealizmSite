bc60b17c3cb310a24a399f9ef1749f3c9f86a73f
RealizmModz <74314445+RealismModz@users.noreply.github.com>
2022-02-14 18:53:36 +0000

    Fixing

--- src/server.js
+++ src/server.js
@@ -1,30 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080
-const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080;
+const path = require("path");
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+app.use(express.static("src/static"));
 
-app.use(express.static('/static'))
-
-app.set('views','src/views');
-app.set('view engine', 'ejs');
-
-app.get('/',(req,res) => {
-	res.redirect('/getstarted');
+app.get("/", (req, res) => {
+  res.redirect("/getstarted");
 });
 
-app.get('/getstarted',(req,res) => {
-  res.render('getstarted');
+app.get("/getstarted", (req, res) => {
+  res.render("getstarted");
 });
 
-app.listen(PORT,function(err) {
-	if(err) return console.log(err);
 
-	mongoose.connect(process.env.MONGO_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-).then(console.log('Connected to DB'))
-
-	console.log(`Listening on port ${PORT}`);
+app.listen(PORT, function (err) {
+  if (err) return console.log(err);
+  console.log(`Successfully Listening on ${PORT}`);
 });
