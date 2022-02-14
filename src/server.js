1e12ee033cd28c0accf2c469409f6c2aab694b90
RealizmModz <74314445+RealismModz@users.noreply.github.com>
2022-02-14 09:41:11 -0600

    Views Setup

--- src/server.js
+++ src/server.js
@@ -3,9 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 8080
 const mongoose = require('mongoose');
 
+app.use(express.static('/static'))
+
 app.get('/',(req,res) => {
-	res.send('Test');
-})
+	res.redirect('/getstarted');
+});
+
+app.get('/getstarted',(req,res) => {
+  res.render('getstarted');
+});
 
 app.listen(PORT,function(err) {
 	if(err) return console.log(err);
